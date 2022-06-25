b76ce1b3138af9407ec4ca24cea8ed12ea64e20b
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-06-25 13:40:52 +0500

    2 homework

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,34 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.conf import settings
+import csv
 
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
+CONTENT = []
+with open(settings.BUS_STATION_CSV, encoding='utf-8') as file:
+    file_reader = csv.reader(file, delimiter=",")
+    count = 0
+    for row in file_reader:
+        if count != 0:
+            data = (
+                row[1],
+                row[4],
+                row[6]
+            )
+            CONTENT.append(data)
+        count += 1
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
 
+    page_number = int(request.GET.get('page',1))
+    paginator = Paginator(CONTENT, 15)
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'page': page,
     }
+
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -19,12 +19,12 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def recipe(request, recipe_name):
+    portion = int(request.GET.get('servings', 1))
+    context = {
+        'recipe':{
+            x: DATA[recipe_name][x]*portion for x in DATA[recipe_name]
+        }
+    }
+    return render(request, 'calculator/index.html', context)
+
