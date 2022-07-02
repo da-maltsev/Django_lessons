f04b4e94eb7afa27e093ef263860b167fb3de46b
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-07-02 13:03:59 +0500

    ORM 2.1.1

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.utils.timezone import now
+from autoslug import AutoSlugField
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.URLField()
+    release_date = models.DateField(default=now())
+    lte_exists = models.BooleanField(default=None)
+    slug = AutoSlugField(populate_from='name', unique=True)
     pass
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +8,19 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    context = {'phones': Phone.objects.all()}
+    sorting_get = request.GET.get('sort')
+    sorting = {
+        'name':'name',
+        'min_price':'price',
+        'max_price':'-price'
+    }
+    if sorting_get:
+        context={'phones':Phone.objects.all().order_by(sorting[sorting_get])}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {'phone':Phone.objects.filter(slug=slug).first()}
     return render(request, template, context)
