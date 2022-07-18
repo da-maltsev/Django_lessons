47c38605e4425c427b8bea4a048a8149af5351ee
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-07-18 20:22:36 +0500

    3.2. DB

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -80,9 +80,11 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_stocks_products',
+        'NAME': 'netology_import_phones',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '0000',
     }
 }
 
